4d8762b78dc38bb69bd133d37428d917e5ac6d28
nehagoswami2403 <nehagoswami2403@gmail.com>
2023-10-13 01:08:17 +0530

    useEffect and fetch live api, load-render-apicall-re-render concept

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,28 @@
 import RestaurentComp from "./RestaurentCard";
 import restList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-    const [restaurentList, setRestaurentList] = useState(restList);
+    const [restaurentList, setRestaurentList] = useState([]);
     // const arr = useState(restList);
     // const [restaurentList, setRestaurentList] = arr - array destructiong, we can write code like ths as well 
     //                                                 its same code.
+
+    useEffect(() => {
+       fetchData();
+    },[]);
+
+    const fetchData = async () => {
+        const data =  await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+        const json = await data.json();
+        console.log(json);
+        //calling useState to re render the comp
+        //optional chaining
+        setRestaurentList(json.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }
+
     return (
       <div className="body">
         <div className="filter">
