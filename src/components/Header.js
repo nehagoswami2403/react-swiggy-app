50618a455dff4d547bce8484025982ebb4e8d1a4
nehagoswami2403 <nehagoswami2403@gmail.com>
2023-10-14 22:24:14 +0530

    added link to navigate comp which is client side routing(episode-7 part-4)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { LOGO_URL } from "../utils/constants";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
     const [btnName, setBtnName] = useState('Login')
@@ -10,9 +11,15 @@ export const Header = () => {
           </div>
           <div className="nav-items">
             <ul> 
-              <li>Home</li>
-              <li>About Us</li>
-              <li>Order</li>
+              <li>
+                <Link to="/">Home</Link>
+            </li>
+              <li>
+                <Link to="/about">About</Link>
+             </li>
+              <li>
+               <Link to="/contact">Contact</Link> 
+             </li>
               <li>Cart</li>
               <button className="login-btn" onClick={() => btnName==="Login"
                ? setBtnName('Logout') : setBtnName('Login')} >{btnName}</button>
