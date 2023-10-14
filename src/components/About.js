06c18c54fb5f9e44c5c830a6876adc7d93f5b9c1
nehagoswami2403 <nehagoswami2403@gmail.com>
2023-10-14 21:49:24 +0530

    added routers for about, contact and error page(react-router-dom) episode-7, part-3

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div className="about">
+            <h1>About</h1>
+            <h2>this is swiggy app</h2>
+        </div>
+    )
+}
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDom from 'react-dom/client';
 import { Header } from './components/Header';
 import Body from './components/Body';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error'
 
 const AppLayout = () => {
     return (
@@ -11,9 +15,25 @@ const AppLayout = () => {
         </div>
     );
 };
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />
+  },
+  {
+    path: "/about",
+    element: <About />
+  },
+  {
+    path: "contact",
+    element: <Contact />
+  }
+]);
   
 const root = ReactDom.createRoot(document.getElementById("root"));
 
 root.render(
-      <AppLayout />
+      <RouterProvider router={appRouter} />
 );
