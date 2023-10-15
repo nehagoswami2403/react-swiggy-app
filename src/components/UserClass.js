322205d2897c0b22df6d2f73a46eb743ca262e31
nehagoswami2403 <nehagoswami2403@gmail.com>
2023-10-15 18:55:03 +0530

    local state and setState for class based comp
    episode-8, part-3

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,12 +4,22 @@ import React from "react"
 class UserClass extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            count: 0
+        }
     }
     render() {
-        const {name, location, contact} = this.props;
-
+        const { name, location, contact } = this.props;
+        const { count, count2 } = this.state;
         return (
             <div className="user-card">
+                <h1>Count: {count}</h1>
+                <button onClick={() => {
+                    this.setState({
+                        count: this.state.count + 1
+                    })
+                    
+                }}>Count increase</button>
                 <h1>Name: {name}</h1>
                 <h2>Location: {location}</h2>
                 <h3>Contact: {contact}</h3>
