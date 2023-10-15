c1125bdf6149ecd82952a35e8c72e7a07af25ace
nehagoswami2403 <nehagoswami2403@gmail.com>
2023-10-15 18:25:35 +0530

    used class based comp and passing props
    used class based comp and passing props - episode-8, part 1

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
+import UserClass from "./UserClass";
+
 const About = () => {
     return (
         <div className="about">
             <h1>About</h1>
             <h2>this is swiggy app</h2>
+            <UserClass name={"Neha"} location={"Bangalore"}
+            contact={"nehagoswami2403"} />
         </div>
     )
 }
---
+++ src/components/UserClass.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+//class based comp
+class UserClass extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        const {name, location, contact} = this.props;
+
+        return (
+            <div className="user-card">
+                <h1>Name: {name}</h1>
+                <h2>Location: {location}</h2>
+                <h3>Contact: {contact}</h3>
+            </div>
+        )
+    }
+}
+
+export default UserClass;
